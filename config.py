377b8abd05dc58619055dda0fc233af31b15da7b
HexaTalkk <163973186+HexaTalkk@users.noreply.github.com>
2024-03-21 00:18:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
-API_ID = API_ID = 29509604
+API_ID = API_ID = 20589899
 
-API_HASH = os.environ.get("API_HASH", "7543d780627ea4a25da5fe5696167440)
+API_HASH = os.environ.get("API_HASH", "62d81c8040fffee65705d0df9931a07b)
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6984025218:AAG00eo_USPsOEuUfExdgwPFChrksaQG5FQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7162386874:AAFfjkS7K2WOpIse1Fl78mF3dHRuYp541fo")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
 OWNER = int(os.environ.get("OWNER", 5971411129))
 
-LOG = -1002039122754
+LOG = -1002042415574
 
 try:
     ADMINS=[]
